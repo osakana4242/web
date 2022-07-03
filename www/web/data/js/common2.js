72e2069fc4ea245f6fee4ebf5eef4b255c0bf29d
osakana4242 <osakana4242@gmail.com>
2022-07-03 15:22:05 +0900

    feat: Google Analytics 4

--- www/web/data/js/common2.js
+++ www/web/data/js/common2.js
@@ -193,26 +193,45 @@
 
 
 //------------------------------------------------------------------------------
-// google.
+// Google Analytics 4.
 
 (function (global) {
-	var _gaq = global._gaq || [];
-	
-	_gaq.push(['_setAccount', 'UA-11051536-4']);
-	_gaq.push(['_trackPageview']);
-	
-	global._gaq = _gaq;
-
 	var host = '' + document.location.host;
 	var isLocalHost = (host === '127.0.0.1') || (host === 'localhost');
 	if (isLocalHost) return;
 	
-	(function() {
+		// <script async src="https://www.googletagmanager.com/gtag/js?id=UA-11051536-4"></script>
+		(function () {
 		var ga = document.createElement('script');
 		ga.type = 'text/javascript';
 		ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		ga.src = 'https://www.googletagmanager.com/gtag/js?id=UA-11051536-4';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(ga, s);
+	})();
+
+	// <script>
+	//   window.dataLayer = window.dataLayer || [];
+	//   function gtag(){dataLayer.push(arguments);}
+	//   gtag('js', new Date());
+	// 
+	//   gtag('config', 'UA-11051536-4');
+	// </script>
+	(function () {
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-11051536-4');
 	})();
 }(window));
-
+// 元のコード.
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=UA-11051536-4"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+// 
+//   gtag('config', 'UA-11051536-4');
+// </script>
